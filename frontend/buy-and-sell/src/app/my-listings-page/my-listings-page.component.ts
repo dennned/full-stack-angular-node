7dd9780e5383ec6acb23c7907381313d25c797f2
Denis <makarenkodeniz@gmail.com>
2021-02-28 22:12:52 +0100

    Modif MyListing Component

--- frontend/buy-and-sell/src/app/my-listings-page/my-listings-page.component.ts
+++ frontend/buy-and-sell/src/app/my-listings-page/my-listings-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeMyListings } from '../fake-data'; 
 
 @Component({
   selector: 'app-my-listings-page',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-listings-page.component.css']
 })
 export class MyListingsPageComponent implements OnInit {
+  listings: Listing[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.listings = fakeMyListings;
+  }
+
+  onDeleteClicked(listingId: string): void {
+    alert(`Deleting your listing with id ${listingId}`);
   }
 
 }
