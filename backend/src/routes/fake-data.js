6baca3c6eab8197cf020040760d577e663789a17
Denis <makarenkodeniz@gmail.com>
2021-03-04 22:42:40 +0100

    Modif fake data

--- backend/src/routes/fake-data.js
+++ backend/src/routes/fake-data.js
@@ -3,16 +3,22 @@ export const fakeListings = [{
     name: 'Old Boat',
     description: 'A very old boat. Bargain price',
     price: 700,
+    user_id: 1,
+    views: 1,
 }, {
     id: '345',
     name: 'Computer',
     description: 'From the 1990s, a classic!',
     price: 50,
+    user_id: 2,
+    views: 0,
 }, {
     id: '456',
     name: 'Basketball Hoop',
     description: 'Good condition, free delivery',
     price: 100,
+    user_id: 3,
+    views: 1,
 }];
 
 export const fakeMyListings = [{
@@ -20,14 +26,20 @@ export const fakeMyListings = [{
     name: 'Bicycle',
     description: 'Bright blue, very fast',
     price: 75,
+    user_id: 1,
+    views: 1,
 }, {
     id: '234',
     name: 'Electric Guitar',
     description: 'Needs new strings, but otherwise in very good condition',
     price: 99,
+    user_id: 2,
+    views: 1,
 }, {
     id: '987',
     name: 'Playstation',
     description: 'Old playstation, still works though',
     price: 75,
+    user_id: 3,
+    views: 1,
 }];
